7d1ff4aab4f9b9e8807a3b9bff08429b2f5990ec
haukao <Olaussen99>
2021-10-06 16:59:06 +0200

    things

--- assignment2/Queries.py
+++ assignment2/Queries.py
@@ -1,6 +1,7 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
 import haversine as hs
+import datetime
 
 
 class Queries:
@@ -71,30 +72,45 @@ class Queries:
     # TASK 6
     def covid_19_tracking(self):
         print("\n TASK 6 \n")
-        query = "SELECT t1.id, t2.id, t1.user_id, t2.user_id FROM activity AS t1 JOIN activity AS t2 ON (t1.transportation_mode = NULL OR t2.transportation_mode = NULL OR t1.transportation_mode = t2.transportation_mode AND(DATE_SUB(t1.start_date_time, INTERVAL 30 second) > t2.start_date_time AND DATE_SUB(t1.start_date_time, INTERVAL 30 second) < t2.end_date_time) OR (DATE_ADD(t1.end_date_time, INTERVAL 30 second) > t2.start_date_time AND DATE_ADD(t1.end_date_time, INTERVAL 30 second) < t2.start_date_time) OR (DATE_ADD(t1.end_date_time, INTERVAL 30 second) > t2.end_date_time AND DATE_SUB(t1.start_date_time, INTERVAL 30 second) < t2.start_date_time))"
+        query = "SELECT a1.id, a2.id, a1.user_id, a2.user_id FROM activity AS a1, activity AS a2 WHERE (a2.start_date_time BETWEEN DATE_SUB(a1.start_date_time, interval 1 minute) AND DATE_ADD(a1.end_date_time, interval 1 minute) OR a2.end_date_time BETWEEN DATE_SUB(a1.start_date_time, interval 1 minute) AND DATE_ADD(a1.end_date_time, interval 1 minute)) AND a1.id != a2.id AND a1.user_id != a2.user_id"
+        print("Fetching overlapping activities in time \n")
         self.cursor.execute(query)
         activities_within_60_sec = self.cursor.fetchall()
+        print("Calculating times and distance")
         users = []
-        for i, res in enumerate(activities_within_60_sec):
-            print("Activity number:", i)
-            self.cursor.execute(
-                "SELECT * FROM trackpoint WHERE activity_id = {}".format(res[0]))
+        for i in range(len(activities_within_60_sec)):
+            print("Checking activity:", i+1)
+            a1 = activities_within_60_sec[i][0]
+            a2 = activities_within_60_sec[i][1]
+            u1 = activities_within_60_sec[i][2]
+            u2 = activities_within_60_sec[i][3]
+            u1_already_in = u1 in users
+            u2_already_in = u2 in users
+            if u1_already_in and u2_already_in:
+                continue
+            self.cursor.execute("SELECT lat, lon, date_time FROM trackpoint WHERE activity_id = {}".format(a1))
             a1_trackpoints = self.cursor.fetchall()
-            self.cursor.execute(
-                "SELECT * FROM trackpoint WHERE activity_id = {}".format(res[1]))
+            self.cursor.execute("SELECT lat, lon, date_time FROM trackpoint WHERE activity_id = {}".format(a2))
             a2_trackpoints = self.cursor.fetchall()
             for t1 in a1_trackpoints:
                 for t2 in a2_trackpoints:
-                    distance_between = distance((t1[2], t1[3]), (t2[2], t2[3]))
-                    if distance_between <= 100:
-                        users.append(res[2])
-                        users.append(res[3])
+                    close_time = abs((t1[2]- t2[2]).total_seconds()) < 60
+                    if not close_time:
+                        continue
+                    close_space = distance((t1[0], t1[1]), (t2[0], t2[1])) < 0.1
+                    if close_space:
+                        if not u1_already_in:
+                            users.append(u1)
+                        if not u2_already_in:
+                            users.append(u2)
                         break
-        user_ids = len(set(users))
-        print(
-            "The users who have been close to another both in time and space are:", user_ids)
-        print("There are", len(user_ids), "users in the list")
-        return user_ids
+        print(users)
+
+
+
+        trackpoints = self.cursor.fetchall()
+        
+        print(len(activities_within_60_sec))
 
     # TASK 7
     def never_taken_taxi(self):
@@ -142,6 +158,7 @@ class Queries:
 
     # TASK 10
     def distance_walked_in_2008(self):
+        print("\n TASK 10 \n")
         query = "SELECT activity_id, lat, lon FROM trackpoint JOIN (SELECT id FROM activity WHERE user_id = 112 AND transportation_mode = 'walk' AND YEAR(start_date_time)= 2008) AS activities WHERE YEAR(trackpoint.date_time) = 2008 AND trackpoint.activity_id = activities.id;"
         self.cursor.execute(query)
         result = self.cursor.fetchall()
@@ -164,6 +181,7 @@ class Queries:
 
     # TASK 11
     def most_altitude_gained(self):
+        print("\n TASK 11 \n")
         query = "SELECT a1.user_id, SUM(altitude_gained) * 0.3048 FROM (SELECT t1.activity_id AS aid, SUM(t2.altitude - t1.altitude) AS altitude_gained FROM trackpoint AS t1 JOIN  trackpoint AS t2 ON  t1.id = t2.id - 1 WHERE t2.altitude > t1.altitude AND t1.altitude != -777 AND t2.altitude != -777 GROUP BY t1.activity_id) AS tab, activity AS a1 WHERE aid = a1.id GROUP BY a1.user_id ORDER BY SUM(altitude_gained) DESC LIMIT 20;"
         self.cursor.execute(query)
         result = self.cursor.fetchall()
@@ -173,6 +191,7 @@ class Queries:
 
     # TASK 12
     def invalid_activities(self):
+        print("\n TASK 12 \n")
         query = "SELECT activity.user_id, COUNT(DISTINCT(activity.id)) AS Invalid FROM activity JOIN activity AS a2 ON activity.user_id = a2.user_id JOIN trackpoint AS t1 ON t1.activity_id = activity.id JOIN trackpoint AS t2 ON t1.id = t2.id - 1 WHERE t1.activity_id = t2.activity_id AND timestampdiff(SECOND, t1.date_time, t2.date_time) >= 300 GROUP BY activity.user_id ORDER BY COUNT(DISTINCT activity.id) DESC"
         self.cursor.execute(query)
         result = self.cursor.fetchall()
@@ -203,7 +222,7 @@ def main():
     # q.find_duplicate_activities()
 
     # TASK 6
-    # q.covid_19_tracking()
+    q.covid_19_tracking()
 
     # TASK 7
     # q.never_taken_taxi()
@@ -224,7 +243,7 @@ def main():
     #q.most_altitude_gained()
 
     # Task 12
-    q.invalid_activities()
+    #q.invalid_activities()
 
 
 main()
