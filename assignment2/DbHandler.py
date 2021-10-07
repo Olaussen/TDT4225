cbb46899e8929e03ee6967053fb1ceda7e71951e
haukao <Olaussen99>
2021-10-07 16:42:53 +0200

    updated dbhandler

--- assignment2/DbHandler.py
+++ assignment2/DbHandler.py
@@ -1,4 +1,8 @@
+from re import A
 from DbConnector import DbConnector
+from tabulate import tabulate
+import mysql.connector
+from mysql.connector import errorcode
 from pickle import load
 
 
@@ -10,7 +14,7 @@ class DbHandler:
         self.users = []
         self.activities = []
         self.trackpoints = []
-        self.load_preprocessed_data(users = True, activities = True, trackpoints = True)
+        self.load_preprocessed_data()
 
     def load_preprocessed_data(self, users = True, activities = True, trackpoints = True):
         if users:
@@ -28,36 +32,77 @@ class DbHandler:
             with open('trackpoints.pickle', 'rb') as file:
                 self.trackpoints = load(file)
 
+    def create_all_tables(self):
+        TABLES = {}
+        TABLES['user'] = (
+            "CREATE TABLE `user` ("
+            " `id` varchar(10) NOT NULL,"
+            " `has_labels` tinyint DEFAULT NULL,"
+            " PRIMARY KEY (`id`),"
+            " UNIQUE KEY `id_UNIQUE` (`id`)"
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+        )
+        TABLES['activity'] = (
+            "CREATE TABLE `activity` ("
+            " `id` bigint NOT NULL AUTO_INCREMENT,"
+            " `user_id` varchar(10) NOT NULL,"
+            " `transportation_mode` varchar(45) DEFAULT NULL,"
+            " `start_date_time` datetime DEFAULT NULL,"
+            " `end_date_time` datetime DEFAULT NULL,"
+            " PRIMARY KEY (`id`),"
+            " UNIQUE KEY `id_UNIQUE` (`id`),"
+            " KEY `user_id_idx` (`user_id`),"
+            " CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE"
+            ") ENGINE=InnoDB AUTO_INCREMENT=21080101093115105 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
+
+        TABLES['trackpoint'] = (
+            "CREATE TABLE `trackpoint` ("
+            " `id` int NOT NULL AUTO_INCREMENT,"
+            " `activity_id` bigint NOT NULL,"
+            " `lat` double DEFAULT NULL,"
+            " `lon` double DEFAULT NULL,"
+            " `altitude` int DEFAULT NULL,"
+            " `date_days` double DEFAULT NULL,"
+            " `date_time` datetime DEFAULT NULL,"
+            " PRIMARY KEY (`id`),"
+            " UNIQUE KEY `id_UNIQUE` (`id`),"
+            " KEY `activity_id_idx` (`activity_id`),"
+            " CONSTRAINT `activity_id` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE"
+            ") ENGINE=InnoDB AUTO_INCREMENT=9704049 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+            )
+        for table_name in TABLES:
+            table_description = TABLES[table_name]
+            try:
+                print("Creating table {}: ".format(table_name), end='')
+                self.cursor.execute(table_description)
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("already exists.")
+                else:
+                    print(err.msg)
+            else:
+                print("OK")
+    
+    def drop_table(self, table_name):
+        print("Dropping table %s..." % table_name)
+        query = "DROP TABLE %s"
+        self.cursor.execute(query % table_name)
+
     def insert_data_user(self):
-        for user in self.preprocessor.users:
-            # Take note that the name is wrapped in '' --> '%s' because it is a string,
-            # while an int would be %s etc
-            query = "INSERT INTO %s (id, has_labels) VALUES ('%s', %s)"
-            self.cursor.execute(
-                query % ("user", user["id"], user["has_labels"]))
+        query = "INSERT INTO user (id, has_labels) VALUES (%s, %s)"
+        self.cursor.executemany(query, self.users)
         self.db_connection.commit()
 
     def insert_data_activity(self):
-        user = None
-        for activity in self.preprocessor.activities:
-            if not user == activity["user_id"]:
-                user = activity["user_id"]
-                print("Inserting activities for user:", user)
-            query = "INSERT INTO %s (id, user_id, transportation_mode, start_date_time, end_date_time) VALUES (%d, '%s', '%s', '%s', '%s')"
-            self.cursor.execute(query % ("activity", activity["id"], activity["user_id"],
-                                         activity["transportation_mode"], activity["start_date_time"], activity["end_date_time"]))
-            self.db_connection.commit()
-        print("Insertion finished without error!")
+        query = "INSERT INTO activity (id,user_id,transportation_mode,start_date_time,end_date_time) VALUES (%s,%s,%s,%s,%s)"
+        self.cursor.executemany(query, self.activities)
+        self.db_connection.commit()
 
     def insert_data_trackpoint(self):
-        activity = None
-        for trackpoint in self.preprocessor.trackpoints:
-            if not activity == trackpoint["activity_id"]:
-                activity = trackpoint["activity_id"]
-                print("Inserting trackpoints for activity:", activity)
-            query = "INSERT INTO %s (activity_id, lat, lon, altitude, date_days, date_time) VALUES (%d, %s, %s, %d, %s, '%s')"
-            self.cursor.execute(query % ("trackpoint", trackpoint["activity_id"],
-                                         trackpoint["lat"], trackpoint["lon"], trackpoint["altitude"], trackpoint["date_days"], trackpoint["date_time"]))
+        query = "INSERT INTO trackpoint (activity_id,lat,lon,altitude,date_days,date_time) VALUES (%s,%s,%s,%s,%s,%s)"
+        chunks = [self.trackpoints[i:10000+i] for i in range(0, len(self.trackpoints), 10000)]
+        for chunk in chunks:
+            self.cursor.executemany(query, chunk)
             self.db_connection.commit()
         print("Insertion finished without error!")
 
@@ -67,15 +112,25 @@ class DbHandler:
         rows = self.cursor.fetchall()
         # Using tabulate to show the table in a nice way
         return rows
+        
+    def show_tables(self):
+        self.cursor.execute("SHOW TABLES")
+        rows = self.cursor.fetchall()
+        print(tabulate(rows, headers=self.cursor.column_names))
 
 
 def main():
     program = None
     try:
+        
         program = DbHandler()
-        # program.insert_data_user()
-        # program.insert_data_activity()
-        # program.insert_data_trackpoint()
+        #program.create_all_tables()
+        program.drop_table('trackpoint')
+        program.drop_table('activity')
+        program.drop_table('user')
+        #program.insert_data_user()
+        #program.insert_data_activity()
+        #program.insert_data_trackpoint()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
